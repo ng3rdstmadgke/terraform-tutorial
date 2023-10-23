82d4894feb86eaacb724d919483e0185fa8bd675
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-10-23 13:18:28 +0000

    alb

--- sample/envs/dev/main.tf
+++ sample/envs/dev/main.tf
@@ -14,9 +14,9 @@ terraform {
   #   https://developer.hashicorp.com/terraform/language/settings/backends/s3
   backend "s3" {
     # tfstate保存先のs3バケットとキー
-    bucket = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
-    region = "ap-northeast-1"
-    key = "terraform-tutorial/dev/terraform.tfstate"
+    bucket  = "tfstate-store-a5gnpkub"
+    region  = "ap-northeast-1"
+    key     = "terraform-tutorial/dev/terraform.tfstate"
     encrypt = true
     # tfstateファイルのロック情報を管理するDynamoDBテーブル
     #   https://developer.hashicorp.com/terraform/language/settings/backends/s3#dynamodb-state-locking
@@ -30,7 +30,25 @@ provider "aws" {
   # すべてのリソースにデフォルトで設定するタグ
   default_tags {
     tags = {
-      PROJECT_NAME = "TERRAFORM_TUTORIAL"
+      PROJECT_NAME = "TERRAFORM_TUTORIAL_D"
     }
   }
+}
+
+variable "vpc_id" { type = string }
+variable "subnets" { type = list(string) }
+variable "alb_subnets" { type = list(string) }
+
+locals {
+  app_name = "terraform-tutorial"
+  stage    = "dev"
+}
+
+
+module "alb" {
+  source      = "../../modules/alb"
+  app_name    = local.app_name
+  stage       = local.stage
+  vpc_id      = var.vpc_id
+  alb_subnets = var.alb_subnets
 }
---
+++ sample/modules/alb/main.tf
@@ -0,0 +1,77 @@
+variable "app_name" {}
+variable "stage" {}
+variable "vpc_id" {}
+variable "alb_subnets" { type = list(string) }
+variable "ingress_rules_cidr_blocks" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+/**
+ * Output
+ */
+output "alb" {
+  value = aws_lb.app_alb
+}
+
+/**
+ * ALB用セキュリティグループ
+ * aws_security_group: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+ */
+resource "aws_security_group" "app_alb_sg" {
+  name   = "${var.app_name}-${var.stage}-app-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_rules_cidr_blocks
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_rules_cidr_blocks
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_rules_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-${var.stage}-app-alb-sg"
+  }
+}
+
+/**
+ * ALB
+ * aws_alb: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+ */
+resource "aws_lb" "app_alb" {
+  name               = "${var.app_name}-${var.stage}-app-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app_alb_sg.id]
+  subnets            = var.alb_subnets
+  ip_address_type    = "ipv4"
+  idle_timeout       = 60
+  internal           = false
+
+  lifecycle {
+    # 変更を適用しない
+    # https://developer.hashicorp.com/terraform/language/meta-arguments/lifecycle#ignore_changes
+    ignore_changes = all
+    # 強制的なリソースの再作成が起こらないようにする
+    # https://developer.hashicorp.com/terraform/language/meta-arguments/lifecycle#prevent_destroy
+    #prevent_destroy = true
+  }
+}
