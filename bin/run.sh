a26ca255a447bb565025745686b4a08fcd1fb532
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-10-26 08:29:25 +0000

    sampleアプリ実装

--- bin/run.sh
+++ bin/run.sh
@@ -2,5 +2,15 @@
 set -e
 ROOT_DIR="$(cd $(dirname $0)/..; pwd)"
 cd $ROOT_DIR
-docker build --rm -f docker/Dockerfile -t test-app:latest .
-docker run  --rm -ti --network host test-app:latest
+
+docker build \
+  --rm \
+  -f docker/app/Dockerfile \
+  -t terraform-tutorial/local/app:latest .
+
+docker run \
+  --rm \
+  -ti \
+  --network host \
+  -p "80:80" \
+  terraform-tutorial/local/app:latest
