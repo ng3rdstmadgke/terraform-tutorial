dc1a97bfa86bc8610b789fa685d35579331fe111
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-11-27 06:53:06 +0000

    ローカルMySQLサーバ

---
+++ bin/mysqld.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+ROOT_DIR="$(cd $(dirname $0)/..; pwd)"
+cd $ROOT_DIR
+
+ENV_PATH="${ROOT_DIR}/env/local.env"
+
+export $(cat $ENV_PATH | grep -v -e "^ *#")
+
+set -e
+
+docker build \
+  --rm \
+  -f docker/mysql/Dockerfile \
+  -t terraform-tutorial/local/mysql:latest \
+  .
+
+# docker run
+docker rm -f terraform-tutorial-mysql
+
+docker run \
+  -d \
+  --rm \
+  --network host \
+  --name terraform-tutorial-mysql \
+  -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD \
+  terraform-tutorial/local/mysql:latest
+
+docker run \
+  --rm \
+  --network host \
+  --env-file "$ENV_PATH" \
+  terraform-tutorial/local/mysql:latest \
+  /check.sh
