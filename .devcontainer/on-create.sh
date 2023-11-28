2ed32eab9b8e02fe7c6379a2f5dab869304fc183
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-11-28 14:02:44 +0000

    refactor devcontainer.json

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -ex
-echo "=== === === === === === on-create.sh === === === === === ==="
+
 echo "=== === === === === === ls -alF === === === === === ==="
 ls -alF
 echo "=== === === === === === pwd === === === === === ==="
 pwd
-echo "=== === === === === === ls -alF .. === === === === === ==="
-ls -alF ..
 
 PROJECT_ROOT=$(cd $(dirname $0)/..; pwd)
 cd $PROJECT_ROOT
